3a0ad6f792e56f8efdbc518422a94e74bb68d0de
Victor Osinaga <osinagavictor4@gmail.com>
2024-08-12 19:22:55 -0300

    test env react + vite

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const numero = parseInt(import.meta.env.VITE_NUM)
+  const numero2 = import.meta.env.VITE_NUM
+  console.log("numero: ", numero);
+  console.log("numero2: ", numero2);
+  
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + numero)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
